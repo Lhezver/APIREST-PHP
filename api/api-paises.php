373d068193cfdd2702ed83ca399997cdf522e366
Leonardo Insaurralde <lhezver@gmail.com>
2022-03-05 11:41:33 -0300

    v1.1

--- api/api-paises.php
+++ api/api-paises.php
@@ -2,138 +2,166 @@
 require_once 'conexion.php';
 
 try {
-	$conn = new PDO($dsn, $usuario, $contrasenia);
+	$conn = new PDO(DSN, USUARIO, CONTRASENIA);
 
-	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-		if (isset($id) && is_numeric($id)) {
+	switch ($_SERVER['REQUEST_METHOD']) {
+		case 'GET':
+			if (isset($id) && is_numeric($id)) {
 
-			$sentencia = $conn->query('SELECT * FROM paises WHERE paises.id = ' . $id);
-			$sentencia->execute();
-			$result = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+				$sentencia = $conn->query('SELECT * FROM paises WHERE paises.id = ' . $id);
+				$sentencia->execute();
+				$result = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
-			$json = json_encode($result[0], JSON_PRETTY_PRINT);
+				$json = json_encode($result[0], JSON_PRETTY_PRINT);
 
-			$conn = null;
-			$result = null;
+				$sentencia = null;
+				$result = null;
 
-			if ($json == 'null') {
+				if ($json == 'null') {
+					header('Content-Type: application/json');
+					echo 'Not Found';
+					http_response_code(404);
+					$json = null;
+				} else {
+					header('Content-Type: application/json');
+					echo $json;
+					http_response_code(200);
+					$json = null;
+				}
+
+				$id = null;
+			} else if (isset($id)) {
+				$id = null;
+
 				header('Content-Type: application/json');
-				echo 'Not Found';
-				http_response_code(404);
-				$json = null;
+				echo 'Bad Request';
+				http_response_code(400);
 			} else {
+				$sentencia = $conn->query('SELECT * FROM paises');
+				$sentencia->execute();
+				$result = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+				$json = json_encode($result, JSON_PRETTY_PRINT);
+
+				$sentencia = null;
+				$result = null;
+
 				header('Content-Type: application/json');
 				echo $json;
 				http_response_code(200);
+
 				$json = null;
 			}
-		} else if (isset($id)) {
-			header('Content-Type: application/json');
-			echo 'Bad Request';
-			http_response_code(400);
-		} else {
-			$sentencia = $conn->query('SELECT * FROM paises');
-			$sentencia->execute();
-			$result = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+			break;
 
-			$json = json_encode($result, JSON_PRETTY_PRINT);
-
-			$conn = null;
-			$sentencia = null;
-			$result = null;
-
-			header('Content-Type: application/json');
-			echo $json;
-			http_response_code(200);
-			$json = null;
-		}
-	} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-		$postBody = file_get_contents("php://input");
-		$datos = json_decode($postBody, true);
-
-		$sentencia = $conn->prepare('INSERT INTO paises (nombre, habitantes) VALUES (:nombre, :habitantes);');
-		$sentencia->bindParam(':nombre', $datos['nombre'], PDO::PARAM_STR);
-		$sentencia->bindParam(':habitantes', $datos['habitantes'], PDO::PARAM_INT);
-
-		if ($sentencia->execute()) {
-			$conn = null;
-			$sentencia = null;
-			header('Content-Type: application/json');
-			echo 'Created';
-			http_response_code(201);
-		} else {
-			$conn = null;
-			$sentencia = null;
-			header('Content-Type: application/json');
-			echo 'Bad Request';
-			http_response_code(400);
-		}
-	} else if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
-		if (isset($id) && is_numeric($id)) {
-
+		case 'POST':
 			$postBody = file_get_contents("php://input");
 			$datos = json_decode($postBody, true);
 
-			$sentencia = $conn->prepare('UPDATE paises SET paises.nombre = :nombre, paises.habitantes = :habitantes WHERE paises.id = :id;');
+			$sentencia = $conn->prepare('INSERT INTO paises (nombre, habitantes) VALUES (:nombre, :habitantes);');
 			$sentencia->bindParam(':nombre', $datos['nombre'], PDO::PARAM_STR);
 			$sentencia->bindParam(':habitantes', $datos['habitantes'], PDO::PARAM_INT);
-			$sentencia->bindParam(':id', $id, PDO::PARAM_INT);
 
 			if ($sentencia->execute()) {
-				$sqlselect = 'SELECT * FROM paises WHERE paises.id = ' . $id;
-				$result = $conn->query('SELECT * FROM paises WHERE paises.id = ' . $id, PDO::FETCH_ASSOC);
+				$postBody = null;
+				$sentencia = null;
+				$datos = null;
 
 				header('Content-Type: application/json');
-				echo json_encode($result, JSON_PRETTY_PRINT);
-				http_response_code(200);
-
-				$conn = null;
-				$sentencia = null;
-				$result = null;
+				echo 'Created';
+				http_response_code(201);
 			} else {
-				$conn = null;
+				$postBody = null;
 				$sentencia = null;
-				$result = null;
+				$datos = null;
 				header('Content-Type: application/json');
 				echo 'Bad Request';
 				http_response_code(400);
 			}
-		} else {
-			$conn = null;
-			header('Content-Type: application/json');
-			echo 'Bad Request';
-			http_response_code(400);
-		}
-	} else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-		if (isset($id) && is_numeric($id)) {
+			break;
 
-			$sqldelete = 'DELETE FROM paises WHERE paises.id = ' . $id;
+		case 'PUT':
+			if (isset($id) && is_numeric($id)) {
 
-			if ($conn->exec($sqldelete) > 0) {
-				header('Content-Type: application/json');
-				echo 'No Content';
-				http_response_code(204);
+				$postBody = file_get_contents("php://input");
+				$datos = json_decode($postBody, true);
+
+				$sentencia = $conn->prepare('UPDATE paises SET paises.nombre = :nombre, paises.habitantes = :habitantes WHERE paises.id = :id;');
+				$sentencia->bindParam(':nombre', $datos['nombre'], PDO::PARAM_STR);
+				$sentencia->bindParam(':habitantes', $datos['habitantes'], PDO::PARAM_INT);
+				$sentencia->bindParam(':id', $id, PDO::PARAM_INT);
+
+				if ($sentencia->execute()) {
+					$sqlselect = 'SELECT * FROM paises WHERE paises.id = ' . $id;
+					$result = $conn->query('SELECT * FROM paises WHERE paises.id = ' . $id, PDO::FETCH_ASSOC);
+
+					$postBody = null;
+					$sentencia = null;
+					$datos = null;
+					$sqlselect = null;
+
+					header('Content-Type: application/json');
+					echo json_encode($result, JSON_PRETTY_PRINT);
+					http_response_code(200);
+					$result = null;
+				} else {
+					$postBody = null;
+					$sentencia = null;
+					$datos = null;
+					header('Content-Type: application/json');
+					echo 'Bad Request';
+					http_response_code(400);
+				}
+
+				$id = null;
 			} else {
 				header('Content-Type: application/json');
-				echo 'Not Found';
-				http_response_code(404);
+				echo 'Bad Request';
+				http_response_code(400);
 			}
-		} else {
+			break;
+
+		case 'DELETE':
+			if (isset($id) && is_numeric($id)) {
+
+				$sqldelete = 'DELETE FROM paises WHERE paises.id = ' . $id;
+
+				if ($conn->exec($sqldelete) > 0) {
+
+					header('Content-Type: application/json');
+					echo 'No Content';
+					http_response_code(204);
+				} else {
+					header('Content-Type: application/json');
+					echo 'Not Found';
+					http_response_code(404);
+				}
+
+				$sqldelete = null;
+				$id = null;
+			} else {
+				header('Content-Type: application/json');
+				echo 'Bad Request';
+				http_response_code(400);
+			}
+			break;
+
+		default:
 			header('Content-Type: application/json');
-			echo 'Bad Request';
-			http_response_code(400);
-		}
-		$conn = null;
-	} else {
-		header('Content-Type: application/json');
-		echo 'Method Not Allowed';
-		http_response_code(405);
+			echo 'Method Not Allowed';
+			http_response_code(405);
+			break;
 	}
 
 	$conn = null;
-} catch (PDOException $e) {
+} catch (PDOException $pdoexc) {
 	header('Content-Type: application/json');
 	echo 'Internal Server Error';
 	http_response_code(500);
 }
+catch (Exception $exc){
+	header('Content-Type: application/json');
+	echo 'Internal Server Error';
+	http_response_code(500);
+}
+?>
--- api/conexion.php
+++ api/conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$dsn = 'mysql:host=localhost;dbname=api;charset=UTF8';
-	$usuario = 'root';
-	$contrasenia = 'root';
+	const DSN = 'mysql:host=localhost;dbname=api;charset=UTF8';
+	const USUARIO = 'root';
+	const CONTRASENIA = 'c6NqC3YSu6ezh8a8';
 ?>
